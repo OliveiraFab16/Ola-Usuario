d21e50422f074a751efbb51271b39906199e391f
oliveiraFab16 <fabriciocarvalho932@gmail.com>
2020-09-18 15:52:16 -0300

    ola usuario

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Ola_Usuario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nomeDoUsuario;
+            //<---Inicio(Digite--->//
+            Console.Write("Digite Seu Nome: ");
+            nomeDoUsuario = Console.ReadLine();
+            Console.Write("\n");
+            Console.WriteLine($"Olá, {nomeDoUsuario}!");
+            //<---Fim(Digite)--->//
+
+            //<---Inicio(Sair)--->//
+            Console.WriteLine("Pressione ENTER para sair");
+            Console.ReadKey();
+            //<---Fim(Sair)--->//
+            
+        }
+    }
+}
